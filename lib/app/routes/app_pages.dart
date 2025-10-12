7b99c7afcba122bad2bccf2af583ad5cbd794b3d
kzshaown <kzshaown@gmai,.com>
2025-10-12 12:33:33 +0600

    creating screen

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,11 @@
 import 'package:get/get.dart';
 
+import '../modules/checking_position/bindings/checking_position_binding.dart';
+import '../modules/checking_position/views/checking_position_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/splash_screen/bindings/splash_screen_binding.dart';
+import '../modules/splash_screen/views/splash_screen_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +19,16 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH_SCREEN,
+      page: () => const SplashScreenView(),
+      binding: SplashScreenBinding(),
+    ),
+    GetPage(
+      name: _Paths.CHECKING_POSITION,
+      page: () => const CheckingPositionView(),
+      binding: CheckingPositionBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,13 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const SPLASH_SCREEN = _Paths.SPLASH_SCREEN;
+  static const CHECKING_POSITION = _Paths.CHECKING_POSITION;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const SPLASH_SCREEN = '/splash-screen';
+  static const CHECKING_POSITION = '/checking-position';
 }
